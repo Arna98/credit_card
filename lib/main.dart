e60733254767c8bd50132d314559e7bd3e3121c4
arna <alireza.real98@gmail.com>
2023-05-07 11:38:06 +0330

    create cCards Widget and implement build collapsed methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,173 @@
 import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(Home());
 }
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(body: cCards()),
+    );
+  }
+}
+
+class cCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // title
+    Widget buildCollapsed1() {
+      return Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Credit Cards",
+            ),
+          ],
+        ),
+      );
+    }
+
+    // collapsed credit cards
+    buildCollapsed2() {
+      double size = 380;
+      return Stack(alignment: Alignment.centerLeft, children: [
+        Image.asset(width: size, height: size, "assets/images/1.png"),
+        Image.asset(
+            width: size / 1.2, height: size / 1.2, "assets/images/2.png"),
+        Image.asset(
+            width: size / 1.5, height: size / 1.5, "assets/images/3.png"),
+        Image.asset(width: size / 2, height: size / 2, "assets/images/4.png"),
+      ]);
+    }
+
+    buildExpanded1() {
+      return Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Expandable",
+            ),
+            Text(
+              "3 Expandable widgets",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      );
+    }
+
+    buildExpanded2() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              //Expanded(child: buildImg(Colors.lightGreenAccent, 100)),
+              //Expanded(child: buildImg(Colors.orange, 100)),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              // Expanded(child: buildImg(Colors.lightBlue, 100)),
+              //Expanded(child: buildImg(Colors.cyan, 100)),
+            ],
+          ),
+        ],
+      );
+    }
+
+    buildExpanded3() {
+      return Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "loremIpsum",
+              softWrap: true,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ExpandableNotifier(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      child: ScrollOnExpand(
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expandable(
+                collapsed: buildCollapsed1(),
+                expanded: buildExpanded1(),
+              ),
+              Expandable(
+                collapsed: buildCollapsed2(),
+                expanded: buildExpanded2(),
+              ),
+              Expandable(
+                collapsed: buildCollapsed3(),
+                expanded: buildExpanded3(),
+              ),
+              Divider(
+                height: 1,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Builder(
+                    builder: (context) {
+                      var controller =
+                          ExpandableController.of(context, required: true)!;
+                      return TextButton(
+                        child: Text(
+                          controller.expanded ? "COLLAPSE" : "EXPAND",
+                          style: Theme.of(context)
+                              .textTheme
+                              .button!
+                              .copyWith(color: Colors.deepPurple),
+                        ),
+                        onPressed: () {
+                          controller.toggle();
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
